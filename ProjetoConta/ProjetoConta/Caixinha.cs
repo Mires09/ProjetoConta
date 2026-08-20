f7004b7311e9e150e0c5dd6cb3f939c49f82428f
Thamires <152615355+Mires09@users.noreply.github.com>
2026-08-19 21:11:22 -0300

    Add files via upload

---
+++ ProjetoConta/ProjetoConta/Caixinha.cs
@@ -0,0 +1,4 @@
+﻿class Caixinha : Conta
+{
+    public decimal lucro;
+}
---
+++ ProjetoConta/ProjetoConta/Corrente.cs
@@ -0,0 +1,15 @@
+﻿class Corrente : Conta
+{
+    public decimal limite;
+
+    public bool Transfere(decimal valor, Conta conta)
+    {
+        if (Saca(valor))
+        {
+            conta.Deposita(valor);
+            return true;
+        }
+
+        return false;
+    }
+}
